1aa407c3d752f866bd3e8f8e4e3cbe6f7c23af69
johann mifsud <johann@cloudbosses.com>
2022-01-20 23:37:21 +0100

    Admin API

--- lib/clients/admin.ex
+++ lib/clients/admin.ex
@@ -10,8 +10,6 @@ defmodule KeycloakEx.Client.Admin do
       end
 
       def new do
-    #   conf = Application.get_env(:keycloak, Keycloak.Clients.Admin)
-    #    host = Application.get_env(:keycloak, :host_uri)
         conf = config()
 
         OAuth2.Client.new([
@@ -26,6 +24,53 @@ defmodule KeycloakEx.Client.Admin do
         conf = config()
         OAuth2.Client.get_token!(new(), [username: conf[:username], password: conf[:password]], headers)
       end
-    end
+
+      def child_spec() do
+        conf = config()
+        {
+          Finch,
+          name: __MODULE__,
+          pools: %{
+            :default => [size: 10],
+            "#{conf[:host_uri]}" => [size: 32, count: 8]
+          }
+        }
+      end
+
+      defp response({:ok, resp}), do: Jason.decode!(resp.body)
+      defp response(resp), do: resp
+
+      def get_request(url, body \\ nil) do
+        conf = config()
+
+        :get
+        |> Finch.build(
+            "#{conf[:host_uri]}/auth/admin/realms/#{conf[:realm]}/#{url}",
+            [
+              {"Authorization", "Bearer #{get_token().token.access_token}"},
+              {"Accept", "application/json"}
+            ],
+            body
+          )
+        |> Finch.request(__MODULE__)
+        |> response
+      end
+
+      def get_users() do
+        get_request("users")
+      end
+
+      def get_user(id) do
+        get_request("users/#{id}")
+      end
+
+      def get_user_count() do
+        get_request("users/count")
+      end
+
+      def get_users_profile() do
+        get_request("users/profile")
+      end
+     end
   end
 end
