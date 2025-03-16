e339068b189519583fcb90ae9e17d9f854b39bd5
andimon <andrevella2001@gmail.com>
2025-03-16 22:07:26 +0100

    test: set up user and admin with randomly generated keycloak host port

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -2,11 +2,32 @@ defmodule KeycloakEx.TestHelpers do
   require Logger
   alias Testcontainers.{Container, LogWaitStrategy}
 
-  def setup_user_and_admin_config() do
+  def setup_user_and_admin_config(keycloak_host_port) do
     Logger.info("[KeycloakEx][Tests] Setting user and admin config")
+    host_uri = "http://localhost:#{keycloak_host_port}"
+    put_test_user_env(host_uri)
+    put_test_admin_env(host_uri)
+  end
 
-    Config.Reader.read!("test/support/config.exs")
-    |> Application.put_all_env()
+  defp put_test_user_env(host_uri) do
+    Application.put_env(:keycloak_ex, KeycloakEx.TestUser,
+      realm: "test",
+      client_id: "test_client",
+      site: "http://localhost:4000",
+      scope: "test_scope",
+      client_secret: "vHcvRhDrMakeXviFj2SnC5zLjDjEXJEJ",
+      host_uri: host_uri
+    )
+  end
+
+  def put_test_admin_env(host_uri) do
+    Application.put_env(:keycloak_ex, KeycloakEx.TestAdmin,
+      realm: "master",
+      username: "admin",
+      password: "test123!",
+      client_id: "admin-cli",
+      host_uri: host_uri
+    )
   end
 
   def setup_postgres_test_container() do
@@ -67,33 +88,58 @@ defmodule KeycloakEx.TestHelpers do
     |> Container.with_environment(:KC_DB_USERNAME, "postgres")
     |> Container.with_environment(:KC_DB_PASSWORD, "postgres")
     |> Container.with_exposed_port(8080)
-    |> Container.with_cmd(["start-dev"])
+    |> Container.with_bind_mount(
+      File.cwd!() <> "/test/support/init-scripts/test-realm.json",
+      "/opt/keycloak/data/import/test-realm.json"
+    )
+    |> Container.with_cmd(["start-dev", "--import-realm"])
     |> Container.with_waiting_strategy(
       LogWaitStrategy.new(
         ~r/Running the server in development mode*/,
-        200_000 # time out
+        # time out
+        200_000
       )
     )
   end
 
-  def test_user() do
+  def test_user(keycloak_host_port) do
     [
       realm: "test",
       client_id: "test_client",
       site: "http://localhost:4000",
       scope: "test_scope",
       client_secret: "vHcvRhDrMakeXviFj2SnC5zLjDjEXJEJ",
-      host_uri: "http://localhost:18080"
+      host_uri: "http://localhost:#{keycloak_host_port}"
     ]
   end
 
-  def test_admin() do
+  def test_admin(keycloak_host_port) do
     [
       realm: "master",
       username: "admin",
       password: "test123!",
       client_id: "admin-cli",
-      host_uri: "http://localhost:18080"
+      host_uri: "http://localhost:#{keycloak_host_port}"
     ]
+  end
+
+  def test_admin_oauth2_client(keycloak_host_port) do
+    %OAuth2.Client{
+      authorize_url: "/oauth/authorize",
+      client_id: "admin-cli",
+      client_secret: "",
+      headers: [],
+      params: %{},
+      redirect_uri: "",
+      ref: nil,
+      request_opts: [],
+      serializers: %{"application/json" => Jason},
+      site: "",
+      strategy: OAuth2.Strategy.Password,
+      token: nil,
+      token_method: :post,
+      token_url:
+        "http://localhost:#{keycloak_host_port}/realms/master/protocol/openid-connect/token"
+    }
   end
 end
